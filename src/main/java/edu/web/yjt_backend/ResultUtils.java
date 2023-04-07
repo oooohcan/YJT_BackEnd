ad9bea20d8d79c5ccecf6d73f5f1b74cacec4bf5
zxy <s_zhangxueyong@wps.cn>
2023-04-07 13:16:51 +0800

    增加错误捕获处理器和错误结果生成工具

---
+++ src/main/java/edu/web/yjt_backend/ResultUtils.java
@@ -0,0 +1,47 @@
+package edu.web.yjt_backend;
+
+public class ResultUtils{
+    /**
+     * 成功
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseRespone<T> success(T data){return new BaseRespone<>(0,data,"ok");}
+
+    /**
+     * 失败
+     * @param errorCode
+     * @return
+     */
+    public static BaseRespone error(ErrorCode errorCode) {return new BaseRespone<>(errorCode);}
+
+    /**
+     * 失败
+     * @param errorCode
+     * @param description
+     * @return
+     */
+    public static BaseRespone error(ErrorCode errorCode, String description) {return new BaseRespone<>(errorCode.getcode(),null,errorCode.getMessage(),description);}
+
+    /**
+     * 失败
+     * @param errorCode
+     * @param message
+     * @param description
+     * @return
+     */
+    public static BaseRespone error(ErrorCode errorCode, String message, String description) {return new BaseRespone<>(errorCode.getcode(),null,message,description);}
+
+    /**
+     * 失败
+     * @param code
+     * @param message
+     * @param description
+     * @return
+     */
+    public static BaseRespone error(int code, String message, String description){
+        return new BaseRespone<>(code,null,message,description);
+    }
+
+}
