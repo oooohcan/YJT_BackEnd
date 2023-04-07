2a2d58c937fdb6f023a8cb7b852cbab0d01bf079
zxy <s_zhangxueyong@wps.cn>
2023-04-07 17:20:55 +0800

    增加两个请求体文件

---
+++ src/main/java/edu/web/yjt_backend/UserLoginRequest.java
@@ -0,0 +1,17 @@
+package edu.web.yjt_backend;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户登录请求体
+ */
+@Data
+public class UserLoginRequest implements Serializable{
+
+    private static final long serialVersionUID = 5894280542314451634L;
+
+    private String userAccount;
+    private String userPassword;
+}
---
+++ src/main/java/edu/web/yjt_backend/UserRegisterRequest.java
@@ -0,0 +1,19 @@
+package edu.web.yjt_backend;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户注册请求体
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+
+    private static final long serialVersionUID = 5894280542314451634L;
+
+    private String userAccount;
+    private String userPassword;
+    private String checkPassword;
+    private String inviteCode;
+}
