c6124c11b45f65fa4854e0dba598c69a50340da7
oooohcan <1489332064@qq.com>
2023-06-11 15:58:02 +0800

    增加逻辑删除注解

--- src/main/java/edu/web/yjt_backend/model/domain/Paper.java
+++ src/main/java/edu/web/yjt_backend/model/domain/Paper.java
@@ -1,9 +1,7 @@
 package edu.web.yjt_backend.model.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
@@ -54,6 +52,7 @@ public class Paper implements Serializable {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
 
     /**
